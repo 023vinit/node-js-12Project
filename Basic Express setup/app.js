da02c105d740bb04863431ebe3c2cb3ed695deaa
Binit chandra Jha <023vinit@gmail.com>
2018-10-26 22:04:24 +0545

    my first push

---
+++ Basic Express setup/app.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let path = require('path');
+let bodyParser = require('body-parser');
+let nodemailer = require('nodemailer');
+
+let app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+// app.use(bodyParser().json());
+// app.use(bodyParser.urlencoded({extended:false}))
+app.get('/',(req,res)=>{
+    res.send('<h1>Hello World!!!</h1>');
+})
+app.listen(3000);
+
+console.log(`server is running on port 3000`);
